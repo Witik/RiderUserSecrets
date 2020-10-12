ff0c1c383b63e58f25da623f283fc6eb82a3d587
Maarten Balliauw <maarten@maartenballiauw.be>
2020-10-12 12:25:27 +0200

    Add msbuild support - https://github.com/Witik/RiderUserSecrets/issues/8

--- src/main/kotlin/eu/gillissen/rider/usersecrets/OpenUserSecretsAction.kt
+++ src/main/kotlin/eu/gillissen/rider/usersecrets/OpenUserSecretsAction.kt
@@ -2,75 +2,107 @@ package eu.gillissen.rider.usersecrets
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.actionSystem.PlatformDataKeys
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.fileEditor.FileEditorManager
-import com.intellij.openapi.project.DefaultProjectFactory
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.SystemInfo
 import com.intellij.openapi.vfs.LocalFileSystem
+import com.jediterm.terminal.ui.UIUtil
+import com.jetbrains.rider.run.environment.MSBuildEvaluator
 import java.io.File
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.Task
+import java.util.concurrent.TimeUnit
 import javax.xml.parsers.DocumentBuilderFactory
 
 class OpenUserSecretsAction : AnAction() {
 
-    private var chosenProject: Project = DefaultProjectFactory.getInstance().defaultProject
-    private var id: String = ""
-
-    override fun actionPerformed(actionEvent: AnActionEvent) {
-        val dirs = getSecretsDirectory()
-        val path = "$dirs${File.separatorChar}secrets.json"
-        val file = File(path)
-        if (!file.exists()) {
-            File(dirs).mkdirs()
-            file.createNewFile()
-            file.writeText("{\n" +
-                    "//    \"MySecret\": \"ValueOfMySecret\"\n" +
-                    "}")
-        }
-        val virtualFile = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(file)
-            ?: return
-        FileEditorManager.getInstance(chosenProject).openFile(virtualFile, true)
-    }
-
-    private fun getSecretsDirectory(): String {
-        return if (SystemInfo.isWindows)
-            "${System.getenv("APPDATA")}${File.separatorChar}microsoft${File.separatorChar}UserSecrets${File.separatorChar}$id"
-        else
-            "${System.getenv("HOME")}${File.separatorChar}.microsoft${File.separatorChar}usersecrets${File.separatorChar}$id"
+    companion object {
+        private const val CsProjExtension = "csproj"
+        private const val UserSecretsIdMsBuildProperty = "UserSecretsId"
     }
 
     override fun update(actionEvent: AnActionEvent) {
-        val project = actionEvent.getData(PlatformDataKeys.PROJECT)
 
-        if (project == null || project.isDefault) {
-            actionEvent.presentation.isVisible = false
-            actionEvent.presentation.isEnabled = false
+        val project = CommonDataKeys.PROJECT.getData(actionEvent.dataContext) ?: return
+
+        if (project.isDefault) {
+            actionEvent.presentation.isEnabledAndVisible = false
             return
         }
 
-        val file = actionEvent.getData(PlatformDataKeys.VIRTUAL_FILE)
-        if (file == null || "csproj" != file.extension) {
-            actionEvent.presentation.isVisible = false
-            actionEvent.presentation.isEnabled = false
+        val projectFile = actionEvent.getData(PlatformDataKeys.VIRTUAL_FILE)
+        if (projectFile == null || CsProjExtension != projectFile.extension) {
+            actionEvent.presentation.isEnabledAndVisible = false
             return
         }
 
-        val documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
-        val document = documentBuilder.parse(file.inputStream)
+        val document = DocumentBuilderFactory.newInstance()
+            .newDocumentBuilder()
+            .parse(projectFile.inputStream)
+
         document.documentElement.normalize()
-        val userSecretsIds = document.getElementsByTagName("UserSecretsId")
-        var textContent: String? = null
-        if (userSecretsIds.length > 0) {
-            textContent = userSecretsIds.item(0).textContent
-        }
-        if (textContent.isNullOrEmpty()) {
-            actionEvent.presentation.isEnabled = false
+        val userSecretsIdNodes = document.getElementsByTagName(UserSecretsIdMsBuildProperty)
+        if (userSecretsIdNodes.length == 0) {
+            actionEvent.presentation.isEnabledAndVisible = false
             return
         }
 
-        chosenProject = project
-        id = textContent
+        if (userSecretsIdNodes.item(0).textContent.isNullOrEmpty()) {
+            actionEvent.presentation.isEnabled = false
+            actionEvent.presentation.isVisible = true
+            return
+        }
 
         actionEvent.presentation.isEnabledAndVisible = true
+    }
+
+    override fun actionPerformed(actionEvent: AnActionEvent) {
+
+        val project = CommonDataKeys.PROJECT.getData(actionEvent.dataContext) ?: return
+        if (project.isDefault) return
+
+        val projectFile = actionEvent.getData(PlatformDataKeys.VIRTUAL_FILE) ?: return
+        if (CsProjExtension != projectFile.extension) return
+
+        object : Task.Backgroundable(project, "Retrieving user secrets...", true, DEAF) {
+
+            override fun run(indicator: ProgressIndicator) {
+                val msBuildEvaluator = MSBuildEvaluator.getInstance(project)
+                val msBuildProperties = msBuildEvaluator
+                    .evaluateProperties(MSBuildEvaluator.PropertyRequest(projectFile.path, null, listOf(UserSecretsIdMsBuildProperty)))
+                    .blockingGet(1, TimeUnit.MINUTES)
+                    ?: return
+
+                val secretsId = msBuildProperties[UserSecretsIdMsBuildProperty] ?: return
+
+                ApplicationManager.getApplication().invokeLaterOnWriteThread {
+                    val secretsDirectoryRoot = getSecretsDirectoryRoot()
+                    val secretsDirectory = "$secretsDirectoryRoot${File.separatorChar}$secretsId"
+                    val secretsFile = File("$secretsDirectory${File.separatorChar}secrets.json")
+                    if (!secretsFile.exists()) {
+                        File(secretsDirectoryRoot).mkdirs()
+                        File(secretsDirectory).mkdirs()
+                        secretsFile.createNewFile()
+                        secretsFile.writeText(
+                            "{\n" +
+                                    "//    \"MySecret\": \"ValueOfMySecret\"\n" +
+                                    "}"
+                        )
+                    }
+
+                    LocalFileSystem.getInstance().refreshAndFindFileByIoFile(secretsFile)?.let {
+                        FileEditorManager.getInstance(project).openFile(it, true)
+                    }
+                }
+            }
+        }.queue()
+   }
+
+    private fun getSecretsDirectoryRoot(): String {
+        return if (UIUtil.isWindows)
+            "${System.getenv("APPDATA")}${File.separatorChar}microsoft${File.separatorChar}UserSecrets${File.separatorChar}"
+        else
+            "${System.getenv("HOME")}${File.separatorChar}.microsoft${File.separatorChar}usersecrets${File.separatorChar}"
     }
 }
