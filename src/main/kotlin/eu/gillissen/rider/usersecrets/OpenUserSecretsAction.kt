e5562bbac03dbcc7094f068825b92e7e7623501b
Maarten Balliauw <maarten@maartenballiauw.be>
2021-04-11 13:15:29 +0200

    Updates for Rider 2021.1 (#14)
    * NoSuchMethodError on 2021.1 EAP #13
    
    * Support secrets file in Directory.Build.props #15
    
    * Update to 2021.1 RTM

--- src/main/kotlin/eu/gillissen/rider/usersecrets/OpenUserSecretsAction.kt
+++ src/main/kotlin/eu/gillissen/rider/usersecrets/OpenUserSecretsAction.kt
@@ -1,24 +1,29 @@
+@file:Suppress("UnstableApiUsage")
+
 package eu.gillissen.rider.usersecrets
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.actionSystem.PlatformDataKeys
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.fileEditor.FileEditorManager
-import com.intellij.openapi.vfs.LocalFileSystem
-import com.jetbrains.rider.run.environment.MSBuildEvaluator
-import java.io.File
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.Task
 import com.intellij.openapi.util.SystemInfo
+import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.openapi.vfs.VirtualFile
+import com.jetbrains.rd.platform.util.application
+import com.jetbrains.rider.run.environment.MSBuildEvaluator
+import java.io.File
 import java.util.concurrent.TimeUnit
 import javax.xml.parsers.DocumentBuilderFactory
 
 class OpenUserSecretsAction : AnAction() {
 
     companion object {
-        private const val CsProjExtension = "csproj"
+        private val supportedFileExtensions = arrayOf("csproj", "vbproj", "fsproj")
+        private val supportedFileNames = arrayOf("Directory.Build.props", "Directory.Build.targets")
+
         private const val UserSecretsIdMsBuildProperty = "UserSecretsId"
     }
 
@@ -32,7 +37,7 @@ class OpenUserSecretsAction : AnAction() {
         }
 
         val projectFile = actionEvent.getData(PlatformDataKeys.VIRTUAL_FILE)
-        if (projectFile == null || CsProjExtension != projectFile.extension) {
+        if (projectFile == null || !isFileSupported(projectFile)) {
             actionEvent.presentation.isEnabledAndVisible = false
             return
         }
@@ -63,7 +68,7 @@ class OpenUserSecretsAction : AnAction() {
         if (project.isDefault) return
 
         val projectFile = actionEvent.getData(PlatformDataKeys.VIRTUAL_FILE) ?: return
-        if (CsProjExtension != projectFile.extension) return
+        if (!isFileSupported(projectFile)) return
 
         object : Task.Backgroundable(project, "Retrieving user secrets...", true, DEAF) {
 
@@ -76,7 +81,7 @@ class OpenUserSecretsAction : AnAction() {
 
                 val secretsId = msBuildProperties[UserSecretsIdMsBuildProperty] ?: return
 
-                ApplicationManager.getApplication().invokeLaterOnWriteThread {
+                application.invokeLaterOnWriteThread {
                     val secretsDirectoryRoot = getSecretsDirectoryRoot()
                     val secretsDirectory = "$secretsDirectoryRoot${File.separatorChar}$secretsId"
                     val secretsFile = File("$secretsDirectory${File.separatorChar}secrets.json")
@@ -96,6 +101,13 @@ class OpenUserSecretsAction : AnAction() {
                 }
             }
         }.queue()
+    }
+
+    private fun isFileSupported(projectFile: VirtualFile?): Boolean {
+        if (projectFile == null) return false
+
+        return supportedFileExtensions.any { it.equals(projectFile.extension, ignoreCase = true) } ||
+                supportedFileNames.any { it.equals(projectFile.name, ignoreCase = true) }
     }
 
     private fun getSecretsDirectoryRoot(): String {
