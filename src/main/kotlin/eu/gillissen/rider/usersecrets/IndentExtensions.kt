c38d6fcc991510213be6d60384daf193fc6b415a
Andrew Rublyov <andrewrublyov99@gmail.com>
2021-07-17 22:31:16 +0300

    Fix indent creation for tab-based indentation

--- src/main/kotlin/eu/gillissen/rider/usersecrets/IndentExtensions.kt
+++ src/main/kotlin/eu/gillissen/rider/usersecrets/IndentExtensions.kt
@@ -2,8 +2,8 @@ package eu.gillissen.rider.usersecrets
 
 import com.intellij.psi.codeStyle.CommonCodeStyleSettings
 
-internal fun CommonCodeStyleSettings.IndentOptions.createIndent(level: Int): String {
-    val indentBase = if (USE_TAB_CHARACTER) "\t" else " "
-
-    return indentBase.repeat(level * INDENT_SIZE)
-}
+internal fun CommonCodeStyleSettings.IndentOptions.createIndent(level: Int): String =
+    if (USE_TAB_CHARACTER)
+        "\t".repeat(level * (INDENT_SIZE / TAB_SIZE))
+    else
+        " ".repeat(level * INDENT_SIZE)
